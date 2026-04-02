cce190680e50e15436bf37335074b9a59989231a
avishkar-tech <dateavishkar524@gmail.com>
2026-04-02 18:54:22 +0530

    Create StringBasics.java
    completed string basics

---
+++ StringBasics.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class stringbasics {
+    public static void main(String[] args) {
+        String name = "hello";
+        System.out.println(name.length());
+        System.out.println(name.charAt(0));
+        System.out.println(name.substring(1,4));
+        System.out.println(name.equals("hello"));
+
+    }
+}
